a73e4a543a56ebdff690e9a13406d65c77aec26b
Juan Manuel <arenasrinconj@gmail.com>
2022-03-30 11:58:20 -0500

    Employee´s and park´s controllers added

---
+++ src/main/java/Controlador/ControladorEmpleado.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+import Modelo.Empleado;
+import Modelo.Moto;
+
+/**
+ *
+ * @author USER
+ */
+public class ControladorEmpleado {
+    
+    ControladorParqueadero cp = new ControladorParqueadero();
+    public ControladorEmpleado(){
+        
+    }
+    
+    /**
+     * Metodo para registrar una moto en el parqueadero
+     * @param moto
+     * @param documento
+     * @return true si tuvo éxito, de lo contrario false
+     */
+    public boolean registrarEntradaMoto(Moto moto){
+        Moto auxMo = cp.buscarMoto(moto.getPlaca());
+        
+        if( auxMo == null ){
+            if( moto.getEmpleadoResponsable().getTipo().equals("Responsable de patio")){
+
+                    for( int i=0; i<cp.motos.length ;i++ ){
+                        if( cp.motos[i] == null ){
+                            cp.motos[i] = moto;
+                            return true;
+                        }
+                    }
+
+            }
+        
+        }
+        
+        return false;
+    }
+    
+    /**
+     * 
+     * @param moto
+     * @return 
+     */
+    public boolean registrarSalidaMoto(Moto moto){
+         Moto auxMo = cp.buscarMoto(moto.getPlaca());
+        
+        if( auxMo != null ){
+            if( moto.getEmpleadoResponsable().getTipo().equals("Responsable de patio")){
+
+                    for( int i=0; i<cp.motos.length ;i++ ){
+                        if( cp.motos[i].getPlaca().equals(moto.getPlaca())){
+                            cp.motos[i] = null;
+                            return true;
+                        }
+                    }
+
+            }
+        
+        }
+        return false;
+    }
+    
+}
---
+++ src/main/java/Controlador/ControladorParqueadero.java
@@ -0,0 +1,117 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+import Modelo.Empleado;
+import Modelo.Moto;
+
+/**
+ *
+ * @author USER
+ */
+public class ControladorParqueadero {
+    Moto[] motos;
+    private Empleado[] empleados;
+    
+    public ControladorParqueadero(){
+        motos = new Moto[15];
+        empleados = new Empleado[4];
+    }
+    
+    /**
+     * Metodo para saber si una moto ya está registrada en el parqueadero
+     * @param documento
+     * @return empleado si la encuentra, de lo contrario null
+     */
+    public Moto buscarMoto(String placa){
+        for(int i=0 ; i<motos.length ; i++){
+            if(motos[i].getPlaca().equals(placa)){
+                return motos[i];
+            }
+        }
+        
+        return null;
+    }
+    
+    /**
+     * Metodo para saber si un empleado ya está registrado en el arreglo
+     * @param documento
+     * @return empleado si lo encuentra, de lo contrario null
+     */
+    public Empleado buscarEmpleado(int documento){
+        for(int i=0 ; i<empleados.length ; i++){
+            if(empleados[i].getDocumento() == documento){
+                return empleados[i];
+            }
+        }
+        
+        return null;
+    }
+    
+    /**
+     * Metodo para añadir un empleado al arreglo
+     * @param empleado
+     * @return true si se pudo añadir, de lo contrario false
+     */
+    public boolean añadirEmpleado(Empleado empleado){
+        Empleado aux = buscarEmpleado(empleado.getDocumento());
+        
+        if( aux == null ){
+            for(int i=0 ; i<empleados.length ; i++){
+                if(empleados[i] == null){
+                    empleados[i] = empleado;
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    /**
+     * Metodo para eliminar un empleado del arreglo
+     * @param documento
+     * @return true si pudo eliminarlo, de lo contrario false
+     */
+    public boolean eliminarEmpleado(int documento){
+        Empleado aux = buscarEmpleado(documento);
+        
+        if( aux != null ){
+            for(int i=0 ; i<empleados.length ; i++){
+                if(empleados[i].getDocumento() == documento){
+                    empleados[i] = null;
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    /**
+     * Metodo para editar la información de un empleado
+     * @param documento
+     * @param nuevoNombre
+     * @param nuevoTipo
+     * @param nuevoPuedeRegistrar
+     * @return true si se pudo editar, de lo contrario false
+     */
+    public boolean editarEmpleado(int documento , String nuevoNombre , String nuevoTipo , boolean nuevoPuedeRegistrar){
+        Empleado aux = buscarEmpleado(documento);
+        
+        if( aux != null ){
+            for(int i=0 ; i<empleados.length ; i++){
+                if(empleados[i].getDocumento() == documento){
+                    empleados[i].setNombre(nuevoNombre);
+                    empleados[i].setTipo(nuevoTipo);
+                    empleados[i].setPuedeRegistrar(nuevoPuedeRegistrar);
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+}
